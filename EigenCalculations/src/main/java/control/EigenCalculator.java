e227eaebd424f62b4ccc2a3d06020209efd6666a
priyaVora <prvora89@gmail.com>
2018-04-30 09:49:59 -0600

    Updated Code!

--- EigenCalculations/src/main/java/control/EigenCalculator.java
+++ EigenCalculations/src/main/java/control/EigenCalculator.java
@@ -1,12 +1,12 @@
 package control;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.zip.Inflater;
 
 import model.Matrix;
-import model.Vector;
-import model.VectorOrientation;
 
 public class EigenCalculator {
 	MatrixCalculator matrixCal = new MatrixCalculator();
@@ -14,29 +14,88 @@ public class EigenCalculator {
 
 	public static void main(String[] args) {
 		EigenCalculator cal = new EigenCalculator();
-		Matrix A = new Matrix("A", 2, 2);
-		double[][] a_data = new double[2][2];
-		a_data[0][0] = 1;
-		a_data[0][1] = 6;
-		a_data[1][0] = 5;
-		a_data[1][1] = 2;
-		A.setCurrentMatrix(a_data);
-		Matrix givenVector = new Matrix("givenVector", 2, 1);
-		double[][] vectorData = new double[2][1];
-		vectorData[0][0] = -6;
-		vectorData[1][0] = 5;
-		givenVector.setCurrentMatrix(vectorData);
-		cal.isEigenVector(givenVector, A);
+		Matrix givenMatrix = new Matrix("Given Matrix", 2, 2);
+		double[][] given_data = new double[2][2];
+
+		given_data[0][0] = 1;
+		given_data[0][1] = 6;
+		given_data[1][0] = 5;
+		given_data[1][1] = 2;
+		givenMatrix.setCurrentMatrix(given_data);
+
+		givenMatrix.printMatrix();
+		System.out.println(" ");
+		String[][] lambda_data = cal.lambdaIdentity_UnknowLamda(2);
+		cal.subtraction_UnknownMatrices(givenMatrix, lambda_data);
+
 	}
 
-	public void isEigenValue(Matrix A, double lambda) throws FileNotFoundException, UnsupportedEncodingException {
+	public String[][] lambdaIdentity_UnknowLamda(int identitySize) {
+		int i, j;
+		String a[][] = new String[identitySize][identitySize];
+		for (i = 0; i < identitySize; i++) {
+			for (j = 0; j < identitySize; j++) {
+				if (i == j) {
+					a[i][j] = "λ";
+					System.out.print(" " + a[i][j]);
+				} else {
+					a[i][j] = "0";
+					System.out.print(" " + a[i][j]);
+				}
+			}
+			System.out.print("\n");
+		}
+		return a;
+	}
+
+	public String[][] subtraction_UnknownMatrices(Matrix givenMatrix, String[][] lambdaIdentity) {
+		String[][] newData = new String[givenMatrix.getRow()][givenMatrix.getColumn()];
+		String[][] showWork = new String[givenMatrix.getRow()][givenMatrix.getColumn()];
+		String[][] answer = new String[givenMatrix.getRow()][givenMatrix.getColumn()];
+
+		Matrix subMatrix = new Matrix("" + givenMatrix.getName() + "-" + "Lamnbda Identity Matrix",
+				givenMatrix.getRow(), givenMatrix.getColumn());
+		for (int row = 0; row < givenMatrix.getCurrentMatrix().length; row++) {
+			for (int col = 0; col < givenMatrix.getCurrentMatrix()[row].length; col++) {
+				System.out.println(givenMatrix.getCurrentMatrix()[row][col]);
+				System.out.println(lambdaIdentity[row][col]);
+				showWork[row][col] = "(" + givenMatrix.getCurrentMatrix()[row][col] + ")" + " - " + "("
+						+ lambdaIdentity[row][col] + ")";
+				String sub = "" + givenMatrix.getCurrentMatrix()[row][col] + "-" + lambdaIdentity[row][col];
+				answer[row][col] = "(" + sub + ")";
+				newData[row][col] = sub;
+
+			}
+		}
+		// subMatrix.setCurrentMatrix(newData);
+		System.out.println("\nPrint Subtraction \n");
+		for (int i = 0; i < newData.length; i++) {
+			for (int j = 0; j < newData[i].length; j++) {
+				System.out.print(" " + newData[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+		return null;
+	}
+
+	public void findEigenValues(Matrix givenMatrix) throws IOException {
+		boolean folder = new File(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork").mkdirs();
+		PrintWriter writer = new PrintWriter(
+				System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\FindEigenValues.txt");
+	}
+
+	public void isEigenValue(Matrix A, double lambda) throws IOException {
+		boolean folder = new File(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork").mkdirs();
+		PrintWriter writer = new PrintWriter(
+				System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\IsEigenValue.txt");
+
 		System.out.println("-----------------------------------------------------------------------------------");
 		System.out.println("-----------------------------------------------------------------------------------");
 		System.out.println("-----------------------------------------------------------------------------------");
 		System.out.println(
 				"------------------------------Is " + lambda + " a eigen value for Matrix A? ------------------");
 		Matrix B = new Matrix("B", A.getRow(), A.getColumn());
-		double[][] lambdaIdentity = lambaIdentity(2, lambda);
+		double[][] lambdaIdentity = lambaIdentity(A.getRow(), lambda);
 		B.setCurrentMatrix(lambdaIdentity);
 
 		Matrix subtractedMatrix = matrixCal.subtractMatrices(A, B);
@@ -65,7 +124,10 @@ public class EigenCalculator {
 		System.out.println("-----------------------------------------------------------------------------------");
 	}
 
-	public boolean isEigenVector(Matrix givenVector, Matrix A) {
+	public boolean isEigenVector(Matrix givenVector, Matrix A) throws FileNotFoundException {
+		boolean folder = new File(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork").mkdirs();
+		PrintWriter writer = new PrintWriter(
+				System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\IsEigenVector.txt");
 		Matrix multiplicationMatrix = matrixCal.multipyMatrices(A, givenVector);
 		A.printMatrix();
 		givenVector.printMatrix();
--- EigenCalculations/src/test/java/EigenTest.java
+++ EigenCalculations/src/test/java/EigenTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
@@ -11,7 +12,7 @@ import model.Matrix;
 public class EigenTest {
 
 	@Test
-	public void testEigenValue() throws FileNotFoundException, UnsupportedEncodingException {
+	public void testEigenValue() throws IOException {
 		EigenCalculator cal = new EigenCalculator();
 		Matrix A = new Matrix("A", 2, 2);
 		double[][] a_data = new double[2][2];
@@ -25,7 +26,42 @@ public class EigenTest {
 	}
 
 	@Test
-	public void testEigenVector() {
+	public void testEigenValue3() throws IOException {
+		EigenCalculator cal = new EigenCalculator();
+		Matrix A = new Matrix("A", 2, 2);
+		double[][] a_data = new double[2][2];
+		a_data[0][0] = 3;
+		a_data[0][1] = 2;
+		a_data[1][0] = 3;
+		a_data[1][1] = 8;
+		A.setCurrentMatrix(a_data);
+		double lambda = 2;
+		cal.isEigenValue(A, lambda);
+	}
+
+	@Test
+	public void testEigenValue2() throws IOException {
+		EigenCalculator cal = new EigenCalculator();
+		Matrix A = new Matrix("A", 3, 3);
+		double[][] a_data = new double[3][3];
+		a_data[0][0] = 6;
+		a_data[0][1] = -3;
+		a_data[0][2] = 1;
+
+		a_data[1][0] = 3;
+		a_data[1][1] = 0;
+		a_data[1][2] = 5;
+
+		a_data[2][0] = 2;
+		a_data[2][1] = 2;
+		a_data[2][2] = 6;
+		A.setCurrentMatrix(a_data);
+		double lambda = 5;
+		cal.isEigenValue(A, lambda);
+	}
+
+	@Test
+	public void testEigenVector() throws IOException {
 		EigenCalculator cal = new EigenCalculator();
 		Matrix A = new Matrix("A", 2, 2);
 		double[][] a_data = new double[2][2];
@@ -38,6 +74,51 @@ public class EigenTest {
 		double[][] vectorData = new double[2][1];
 		vectorData[0][0] = -6;
 		vectorData[1][0] = 5;
+		givenVector.setCurrentMatrix(vectorData);
+		cal.isEigenVector(givenVector, A);
+	}
+
+	@Test
+	public void testEigenVector2() throws FileNotFoundException {
+		EigenCalculator cal = new EigenCalculator();
+		Matrix A = new Matrix("A", 2, 2);
+		double[][] a_data = new double[2][2];
+		a_data[0][0] = -3;
+		a_data[0][1] = 1;
+		a_data[1][0] = -3;
+		a_data[1][1] = 8;
+		A.setCurrentMatrix(a_data);
+		Matrix givenVector = new Matrix("givenVector", 2, 1);
+		double[][] vectorData = new double[2][1];
+		vectorData[0][0] = 1;
+		vectorData[1][0] = 4;
+		givenVector.setCurrentMatrix(vectorData);
+		cal.isEigenVector(givenVector, A);
+	}
+
+	@Test
+	public void testEigenVector3() throws IOException {
+		EigenCalculator cal = new EigenCalculator();
+		Matrix A = new Matrix("A", 3, 3);
+		double[][] a_data = new double[3][3];
+		a_data[0][0] = 3;
+		a_data[0][1] = 7;
+		a_data[0][2] = 9;
+
+		a_data[1][0] = -4;
+		a_data[1][1] = -5;
+		a_data[1][2] = 1;
+
+		a_data[2][0] = 2;
+		a_data[2][1] = 4;
+		a_data[2][2] = 4;
+
+		A.setCurrentMatrix(a_data);
+		Matrix givenVector = new Matrix("givenVector", 3, 1);
+		double[][] vectorData = new double[3][1];
+		vectorData[0][0] = 4;
+		vectorData[1][0] = -3;
+		vectorData[2][0] = 1;
 		givenVector.setCurrentMatrix(vectorData);
 		cal.isEigenVector(givenVector, A);
 	}
