16fe124554b9e264b005e44a6d6fd537a3802b43
priyaVora <prvora89@gmail.com>
2018-05-01 08:37:26 -0600

    Functionality of : Is EigenValue Valid? is done!

--- EigenCalculations/src/main/java/control/EigenCalculator.java
+++ EigenCalculations/src/main/java/control/EigenCalculator.java
@@ -1,9 +1,12 @@
 package control;
 
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -17,20 +20,36 @@ public class EigenCalculator {
 	VectorCalculator vcal = new VectorCalculator();
 
 	public static void main(String[] args) {
+		// EigenCalculator cal = new EigenCalculator();
+		// Matrix givenMatrix = new Matrix("Given Matrix", 2, 2);
+		// double[][] given_data = new double[2][2];
+		//
+		// given_data[0][0] = 1;
+		// given_data[0][1] = 6;
+		// given_data[1][0] = 5;
+		// given_data[1][1] = 2;
+		// givenMatrix.setCurrentMatrix(given_data);
+		//
+		// givenMatrix.printMatrix();
+		// System.out.println(" ");
+		// String[][] lambda_data = cal.lambdaIdentity_UnknowLamda(2);
+		// cal.subtraction_UnknownMatrices(givenMatrix, lambda_data);
+
 		EigenCalculator cal = new EigenCalculator();
-		Matrix givenMatrix = new Matrix("Given Matrix", 2, 2);
-		double[][] given_data = new double[2][2];
-
-		given_data[0][0] = 1;
-		given_data[0][1] = 6;
-		given_data[1][0] = 5;
-		given_data[1][1] = 2;
-		givenMatrix.setCurrentMatrix(given_data);
-
-		givenMatrix.printMatrix();
-		System.out.println(" ");
-		String[][] lambda_data = cal.lambdaIdentity_UnknowLamda(2);
-		cal.subtraction_UnknownMatrices(givenMatrix, lambda_data);
+		Matrix A = new Matrix("A", 2, 2);
+		double[][] a_data = new double[2][2];
+		a_data[0][0] = 3;
+		a_data[0][1] = 2;
+		a_data[1][0] = 3;
+		a_data[1][1] = 8;
+		A.setCurrentMatrix(a_data);
+		double lambda = 2;
+		try {
+			cal.isEigenValue(A, lambda);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -88,6 +107,17 @@ public class EigenCalculator {
 				System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\FindEigenValues.txt");
 	}
 
+	public void appendToFile(Writer out, double[][] data) throws IOException {
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				out.append(" " + data[i][j] + " ");
+			}
+			out.append("\r\n");
+		}
+
+		out.append("\r\n");
+	}
+
 	public void isEigenValue(Matrix A, double lambda) throws IOException {
 		boolean folder = new File(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork").mkdirs();
 
@@ -96,36 +126,37 @@ public class EigenCalculator {
 
 		Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileDir), "UTF8"));
 
-		out.append("Step 1: Use forumla (A-λ)v = 0").append("\r\n");
+		out.append("Step 1: Use forumla (A-λI)v = 0").append("\r\n");
 		out.append("Step 2: Use result from formula in matrix row operation with zeros(right of augment matrix)")
 				.append("\r\n");
 		out.append("Does the row operation result to zeros across?").append("\r\n");
-		out.append("If yes: " + lambda + " is valid for this matrix.").append("\r\n");
-		out.flush();
-		out.close();
+		out.append("       If yes: " + lambda + " is valid for this matrix.").append("\r\n");
 
-		// writer.println("Step 1: Use forumla (A-λ)v = 0");
-		// writer.println("Step 2: Use result from formula in matrix row operation with
-		// zeros(right of augment matrix)");
-		// writer.println("Does the row operation result to zeros across?");
-		// writer.println("If yes: " + lambda + "is valid for this matrix.");
+		out.append("-----------------------------------------------------------------------------------")
+				.append("\r\n");
 
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println(
-				"------------------------------Is " + lambda + " a eigen value for Matrix A? ------------------");
-		Matrix B = new Matrix("B", A.getRow(), A.getColumn());
+		out.append("------------------------------Is " + lambda + " a eigen value for Matrix A? ------------------")
+				.append("\r\n");
+
+		Matrix B = new Matrix("λI Matrix:", A.getRow(), A.getColumn());
 		double[][] lambdaIdentity = lambaIdentity(A.getRow(), lambda);
 		B.setCurrentMatrix(lambdaIdentity);
+		out.append("(A-λI)V = 0: ").append("\r\n").append("\r\n");
+		System.out.println("λI Matrix: ");
+		out.append("λI Matrix: ").append("\r\n");
+		out.append("\r\n");
+		appendToFile(out, lambdaIdentity);
 
 		Matrix subtractedMatrix = matrixCal.subtractMatrices(A, B);
-		subtractedMatrix.printMatrix();
+		System.out.println("Subtracted Matrix");
+		out.append("Subtracted Matrix").append("\r\n");
+
+		appendToFile(out, subtractedMatrix.getCurrentMatrix());
 
 		Matrix rowOperationMatrix = new Matrix("Operation", subtractedMatrix.getRow(),
 				subtractedMatrix.getColumn() + 1);
 		double[][] operationData = new double[subtractedMatrix.getRow()][subtractedMatrix.getColumn() + 1];
-		System.out.println("\n(A-LambdaIdentiy)V = 0: ");
+
 		for (int i = 0; i < operationData.length; i++) {
 			for (int j = 0; j < operationData[i].length; j++) {
 				try {
@@ -137,13 +168,28 @@ public class EigenCalculator {
 		}
 
 		rowOperationMatrix.setCurrentMatrix(operationData);
+		System.out.println("Row Operation Matrix");
+		out.append("Row Operation Matrix: ").append("\r\n");
 		rowOperationMatrix.printMatrix();
+		appendToFile(out, rowOperationMatrix.getCurrentMatrix());
 
-		System.out.println("Eigen Value is Valid: " + validEigenValue(rowOperationMatrix));
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println("-----------------------------------------------------------------------------------");
+		out.append("//////////////////////////////MATRIX ROW OPERATION/////////////////////////////////")
+				.append("\r\n");
 
+		double[][] rrefResult = matrixCal.rref(rowOperationMatrix.getCurrentMatrix());
+
+		grabsContentFromFile("ShowWork.txt", "IsEigenValue.txt");
+
+		appendToFile(out, rrefResult);
+		out.append("///////////////////////////////////////////////////////////////////////////////////")
+				.append("\r\n");
+
+		out.append("Eigen Value is Valid: " + validEigenValue(rowOperationMatrix)).append("\r\n");
+		out.append("-----------------------------------------------------------------------------------")
+				.append("\r\n");
+
+		out.flush();
+		out.close();
 	}
 
 	public boolean isEigenVector(Matrix givenVector, Matrix A) throws FileNotFoundException {
@@ -168,6 +214,36 @@ public class EigenCalculator {
 		}
 		System.out.println("Given vector is an eigen vector of the Matrix A.");
 		return true;
+	}
+
+	public void grabsContentFromFile(String file1, String file2) {
+		FileReader fr = null;
+		FileWriter fw = null;
+		try {
+			fr = new FileReader(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\" + file1);
+			fw = new FileWriter(System.getProperty("user.home") + "/Desktop" + "\\MatrixShowWork" + "\\" + file2);
+			int c = fr.read();
+			while (c != -1) {
+				fw.write(c);
+				c = fr.read();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			close(fr);
+			close(fw);
+		}
+	}
+
+	public static void close(Closeable stream) {
+		try {
+			if (stream != null) {
+				stream.close();
+			}
+		} catch (IOException e) {
+			// ...
+		}
 	}
 
 	public boolean validEigenValue(Matrix rowOperationMatrix)
